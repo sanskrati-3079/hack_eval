8848306d9687fdd78c7906582770d9b770ccde4e
sanskrati-3079 <agrawalsanskrati323@gmail.com>
2025-09-16 14:16:35 +0530

    updated css of team

--- judge panel/src/components/Header.jsx
+++ judge panel/src/components/Header.jsx
@@ -19,11 +19,11 @@ const Header = ({ onMenuClick }) => {
                     <span className="hamburger-line"></span>
                     <span className="hamburger-line"></span>
                 </div>
-                <img src='../../public/images/gla.png' alt="GLA University" className="header-logo" />
-                <img src='../../public/images/DC.png' alt="Logo 2" className="header-right-logo dc-logo" />
+                <img src='../../public/images/codoraai.png' alt="Codora AI" className="header-logo" />
+                {/* <img src='../../public/images/DC.png' alt="Logo 2" className="header-right-logo dc-logo" /> */}
             </div>
             <div className="header-right">
-                <img src='../../public/images/sih.png' alt="Logo 1" className="header-right-logo" />
+                {/* <img src='../../public/images/sih.png' alt="Logo 1" className="header-right-logo" /> */}
                 <button 
                     onClick={handleLogout}
                     className="logout-btn"
